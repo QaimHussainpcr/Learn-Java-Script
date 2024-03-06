f483062ba7252dcc85706b32508c87b80ccdbd52
Qaim Hussain <qaimpcr.edu91@gmail.com>
2024-03-06 04:01:12 -0800

    Learn JavaScript

--- ConversionOperation/index.js
+++ ConversionOperation/index.js
@@ -1,53 +1,53 @@
 // ===================== String convert to number ================
 
-let value = "12345";
-console.log(typeof(value));
-let result = Number(value);
-console.log(result)
-console.log(typeof(result))
+// let value = "12345";
+// console.log(typeof(value));
+// let result = Number(value);
+// console.log(result)
+// console.log(typeof(result))
 
 // ===================== Number convert to String ====================
 
-let numb = 12345;
-console.log(typeof(numb));
-let str = String(numb)
-console.log(typeof(str));
-console.log(str)
+// let numb = 12345;
+// console.log(typeof(numb));
+// let str = String(numb)
+// console.log(typeof(str));
+// console.log(str)
 
 // ====================== Boolean to Number & Number to Boolean =================
-let bool = true;
-console.log(typeof(bool));
-let num = Number(bool);
-console.log(num);
-console.log(typeof(num));
+// let bool = true;
+// console.log(typeof(bool));
+// let num = Number(bool);
+// console.log(num);
+// console.log(typeof(num));
 
-let bol = false;
-console.log(typeof(bool));
-let con = Number(bol);
-console.log(con);
-console.log(typeof(con));
-
-
-let number = 1;
-let qaim = Boolean(number);
-console.log(typeof(qaim));
-console.log(qaim);
-
-let numbe = 0;
-let qai = Boolean(numbe);
-console.log(typeof(qai));
-console.log(qai);
-
-let empty = "";
-let qa = Boolean(empty);
-console.log(typeof(qa));
-console.log(qa);
+// let bol = false;
+// console.log(typeof(bool));
+// let con = Number(bol);
+// console.log(con);
+// console.log(typeof(con));
 
 
-let stri = "Qaim Hussain";
-let q = Boolean(stri);
-console.log(typeof(q));
-console.log(q);
+// let number = 1;
+// let qaim = Boolean(number);
+// console.log(typeof(qaim));
+// console.log(qaim);
+
+// let numbe = 0;
+// let qai = Boolean(numbe);
+// console.log(typeof(qai));
+// console.log(qai);
+
+// let empty = "";
+// let qa = Boolean(empty);
+// console.log(typeof(qa));
+// console.log(qa);
+
+
+// let stri = "Qaim Hussain";
+// let q = Boolean(stri);
+// console.log(typeof(q));
+// console.log(q);
 
 
 // ======================= Notes =======================
@@ -65,3 +65,49 @@ console.log(q);
 */
 
 
+
+
+// =========================== Operations =========================
+
+// let digit = 3;
+// let negative = -digit;
+// console.log(negative);
+// console.log(typeof(negative));
+
+
+// ######################### Additional Operations #########################
+let a = 23;
+let b = 12;
+let sum = a + b;
+console.log(sum)
+
+// ######################### Subtraction Operations #########################
+let c = 23;
+let d = 12;
+let sub = c - d;
+console.log(sub);
+
+// ######################### Multiply Operations #########################
+let e = 12;
+let f = 3;
+let mul = e * f;
+console.log(mul);
+
+// ######################### Divide Operations #########################
+let g = 12;
+let h = 4;
+let div = g / h;
+console.log(div);
+
+// ######################### Power Operations #########################
+let i = 2;
+let power = i**4;
+console.log(power);
+
+// ######################### Modulus Operations #########################
+let j = 3;
+let k = 2;
+let mod = j % k;
+console.log(mod)
+
+// ######################### Most Advance & most useable Operations #########################
